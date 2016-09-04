e79bdf88254f27b7a8a42c9594bd363724c5c819
lenLRX <545976176@qq.com>
2016-09-04 09:52:59 +0800

    larger  test

--- main.cpp
+++ main.cpp
@@ -10,21 +10,21 @@ int main(){
 	srand(time(0));
 	BinaryTree tree;
 	std::vector<int> save;
-	for (int i = 0; i < 10; i++){
+	for (int i = 0; i < 10000000; i++){
 		int v = rand();
 		save.push_back(v);
 		tree.put(v);
 	}
 
-	tree.inorder();
+	//tree.inorder();
 
 	for (int v : save)
 		tree.remove(v);
 
-	tree.inorder();
+	//tree.inorder();
 
 
-	system("pause");
+	//system("pause");
 	return 0;
 }
 
